8e6ec1b5709284e8b347dec2d8b98299073be7e5
Caroline Lopes <86009348+caroldeirante@users.noreply.github.com>
2021-10-01 17:57:52 -0300

    Adicionei o AnimeFlix que fiz na Imersão Alura

---
+++ script.js
@@ -0,0 +1,9 @@
+var listaFilmes = [
+  "https://metagalaxia.com.br/wp-content/uploads/2019/10/Os-Personagens-de-Demon-Slayer-Kimetsu-no-Yaiba-capa.jpg",
+  "https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q80/wp-content/uploads/2018/02/legiao_1tjDTZu_YHPEMsagoGfblI96w0XvhWm78KUBSpLxQA.png.jpeg",
+  "https://karinsensei.com/wp-content/uploads/2014/05/Angel-Beats-Capa-01.jpg"
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
